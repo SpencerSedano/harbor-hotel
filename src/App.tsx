80520775670019b54d51c91bc8a9eb38b8034249
資訊PT <mispt@tchhotel.com.tw>
2025-05-26 10:25:43 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+// import { useState } from 'react'
+
+import './App.css'
+
+import HotelLogo from "./assets/logo.svg";
+
+function App() {
+  return (
+    <>
+      <nav className='flex justify-around items-center h-18 bg-transparent z-20 '>
+        <div className='flex'>
+          <li className='list-none'>Menu</li>
+          <li className='list-none'>Languages</li>
+        </div>
+        <div>
+          <img className='w-64 h-64' src={HotelLogo} alt="Logo" />
+        </div>
+        <div>Reserve</div>
+      </nav>
+      <img className='z-10 h-dvh' src="room-1.webp" alt="Room"/>
+    </>
+  )
+}
+
+export default App
